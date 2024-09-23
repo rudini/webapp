7c4450e3e935ba5a3201d67704fb548fdab7daaf
Roger Rudin <rogerrudin@MacBook-Pro-von-Roger.local>
2024-09-23 11:30:48 +0200

    feat(): Fix import and platform check in app.config.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  redirectUrl: 'https://hftm-webapp.azurewebsites.net',
-  postLogoutRedirectUri: 'https://hftm-webapp.azurewebsites.net',
-  silentRenewUrl: 'https://hftm-webapp.azurewebsites.net/silent-renew.html',
+  redirectUrl: 'http://localhost:4000',
+  postLogoutRedirectUri: 'http://localhost:4000',
+  silentRenewUrl: 'http://localhost:4000/silent-renew.html',
   serviceUrl:
-    'https://hftm-webapp.azurewebsites.net/api',
-  authority: 'https://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/realms/blog',
+    'http://localhost:4000/api',
+  authority: 'http://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/realms/blog',
 };
