5b2e099de87c69c503b7ff9076c67e51409f540c
Roger Rudin <rogerrudin@MacBook-Pro-von-Roger.local>
2024-09-24 23:48:10 +0200

    feat(): https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   silentRenewUrl: 'https://hftm-webapp.azurewebsites.net/silent-renew.html',
   serviceUrl:
     'https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io',
-  authority: 'http://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/realms/blog',
+  authority: 'https://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/realms/blog',
 };
